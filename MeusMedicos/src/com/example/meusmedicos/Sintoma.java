e2c59772162ba5614bed37bbc694827adcd25898
Dênnis Dantas <ddspog@gmail.com>
2015-07-30 10:15:12 -0300

    Creating Sintomas class
    The Sintomas class was created, to start implementing Sintomas-related UI.
    After that, I'm gonna start implementing the screen of Sintomas creation.

---
+++ MeusMedicos/src/com/example/meusmedicos/Sintoma.java
@@ -0,0 +1,71 @@
+package com.example.meusmedicos;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Dênnis on 7/30/2015.
+ */
+public class Sintoma implements Comparable<Sintoma> {
+    private String titulo;
+    private int duracaoDeDias;
+    private GregorianCalendar dataQueComecou;
+    private String especialidade;
+    private String anotacao;
+
+    public Sintoma(String titulo, GregorianCalendar date, int duracao, String especialidade) {
+        this.setTitulo(titulo);
+        this.setDataQueComecou(date);
+        this.setDuracaoDeDias(duracao);
+        this.setEspecialidade(especialidade);
+        this.setAnotacao("");
+    }
+
+    public String getTitulo() {  return this.titulo;  }
+
+    public void setTitulo(String titulo) {   this.titulo = titulo;  }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public int getDuracaoDeDias() {
+        return this.duracaoDeDias;
+    }
+
+    public void setDuracaoDeDias(int duracaoDeDias) {  this.duracaoDeDias= duracaoDeDias;  }
+
+    public GregorianCalendar getDataQueComecou() {
+        return this.dataQueComecou;
+    }
+
+    public void setDataQueComecou(GregorianCalendar date) {
+        this.dataQueComecou = date;
+    }
+
+    public String getAnotacao() {
+        return anotacao;
+    }
+
+    public void setAnotacao(String anotacao) {
+        this.anotacao = anotacao;
+    }
+
+    @Override
+    public int compareTo(Sintoma another) {
+        if (this.getDataQueComecou().getTimeInMillis() != another.getDataQueComecou().getTimeInMillis()) {
+            return this.getDataQueComecou().compareTo(another.getDataQueComecou());
+        } else if (!this.getEspecialidade().equals(another.getEspecialidade())) {
+            return this.getEspecialidade().compareTo(another.getEspecialidade());
+        } else if (this.getDuracaoDeDias() != another.getDuracaoDeDias()) {
+            return (this.getDuracaoDeDias() < another.getDuracaoDeDias())? -1 : 1;
+        } else {
+            return this.getTitulo().compareTo(another.getTitulo());
+        }
+
+    }
+
+}
