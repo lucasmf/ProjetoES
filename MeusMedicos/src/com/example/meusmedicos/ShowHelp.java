6e8101822e72e460c8e0c85a3c060b666391e95c
JuliePessoa <julie.pessoa@ccc.ufcg.edu.br>
2015-08-02 16:54:55 -0300

    add showHelp

--- MeusMedicos/src/com/example/meusmedicos/ShowHelp.java
+++ MeusMedicos/src/com/example/meusmedicos/ShowHelp.java
@@ -1,35 +1,24 @@
 package com.example.meusmedicos;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+/**
+ * Created by JULIE on 02/08/2015.
+ */
+public class ShowHelp extends Activity {
 
-public class MainActivity extends Activity {
-
-	public void openSintomas(View view) {
-	    Intent intent = new Intent(this, ShowSintomas.class);
-	    startActivity(intent);
-	}
-	
-	public void openConsultas(View view) {
-	    Intent intent = new Intent(this, ShowConsultas.class);
-	    startActivity(intent);
-	}
-	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.show_help);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.help, menu);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         return true;
     }
@@ -40,11 +29,6 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_help) {
-            Intent intent = new Intent(this, ShowHelp.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
