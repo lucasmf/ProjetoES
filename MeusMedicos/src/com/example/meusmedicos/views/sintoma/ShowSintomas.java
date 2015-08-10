22ee1070b8e7ff0579ecbf62eb74da00d1502da6
odwl66 <orion.lima@ccc.ufcg.edu.br>
2015-08-10 18:00:50 -0300

    Adicionando filtro por de sintomas por especialidade

--- MeusMedicos/src/com/example/meusmedicos/views/sintoma/ShowSintomas.java
+++ MeusMedicos/src/com/example/meusmedicos/views/sintoma/ShowSintomas.java
@@ -3,20 +3,25 @@ package com.example.meusmedicos.views.sintoma;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StringDef;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 
 import com.example.meusmedicos.controllers.Controller;
 import com.example.meusmedicos.Global;
 import com.example.meusmedicos.R;
+import com.example.meusmedicos.models.Especialidade;
 import com.example.meusmedicos.models.Sintoma;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,13 +31,45 @@ import java.util.Map;
  */
 public class ShowSintomas extends Activity {
     private ListView lv;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_sintomas);
         lv = (ListView) findViewById(R.id.listView2);
+        addItemsOnSpinner();
         loadListOfSintomas();
+        spinnerOnChange();
+    }
+
+    public void spinnerOnChange(){
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                loadListOfSintomas();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                loadListOfSintomas();
+            }
+
+        });
+    }
+
+    public void addItemsOnSpinner() {
+
+        spinner = (Spinner) findViewById(R.id.dropdown);
+        ArrayList<String> list = new ArrayList<String>(
+                Arrays.asList("Todos"));
+        for (Especialidade e: Controller.getEspecialidades()){
+            list.add(e.toString());
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
     }
 
     private void openDetalhes(int id) {
@@ -66,18 +103,28 @@ public class ShowSintomas extends Activity {
     protected void onRestart() {
         super.onRestart();
         lv.setAdapter(null);
+        addItemsOnSpinner();
         loadListOfSintomas();
+        spinnerOnChange();
+    }
+
+    private String getSelectedEspecialidade(){
+        return spinner.getSelectedItem().toString();
     }
 
     private void loadListOfSintomas() {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         ArrayList<Sintoma> sintomas = Controller.getSintomas();
         List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+
         for (Sintoma item : sintomas) {
-            Map<String, String> datum = new HashMap<String, String>(2);
-            datum.put("campo1", item.getTitulo());     //Item
-            datum.put("campo2", formatter.format(item.getDataQueComecou().getTime()));    //subItem
-            data.add(datum);
+            if (getSelectedEspecialidade().equals("Todos") ||
+                    item.getEspecialidade().equals(getSelectedEspecialidade())) {
+                Map<String, String> datum = new HashMap<String, String>(2);
+                datum.put("campo1", item.getTitulo());     //Item
+                datum.put("campo2", formatter.format(item.getDataQueComecou().getTime()));    //subItem
+                data.add(datum);
+            }
         }
         SimpleAdapter adapter = new SimpleAdapter(this, data,android.R.layout.simple_list_item_2,new String[] {"campo1", "campo2"},
                 new int[] {android.R.id.text1, android.R.id.text2});
